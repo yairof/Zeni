ecbe5ff099fed0a156f6261e4be4711d7bd8f357
Yairo Fernandez <yairofernandez@Macs-MacBook-Pro-6.local>
2018-11-15 00:30:51 -0500

    Working Calendar Picker

--- Zeni/Controller/ZeniVC.swift
+++ Zeni/Controller/ZeniVC.swift
@@ -14,6 +14,7 @@ class ZeniVC: UIViewController {
     @IBOutlet weak var dateTwo: UITextField!
     
     private var datePicker: UIDatePicker?
+    private var datePickerTwo: UIDatePicker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +23,22 @@ class ZeniVC: UIViewController {
         datePicker?.datePickerMode = .date
         datePicker?.addTarget(self, action: #selector(ZeniVC.dateChanged(datePicker:)), for: .valueChanged)
         
+        datePickerTwo = UIDatePicker()
+        datePickerTwo?.datePickerMode = .date
+        datePickerTwo?.addTarget(self, action: #selector(ZeniVC.dateChangedTwo(datePickerTwo:)), for: .valueChanged)
+        
+        
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ZeniVC.viewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
         dateOne.inputView = datePicker
-        dateTwo.inputView = datePicker
+        dateTwo.inputView = datePickerTwo
+    }
+    
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
     }
     
     @objc func dateChanged(datePicker: UIDatePicker) {
@@ -32,10 +47,20 @@ class ZeniVC: UIViewController {
         dateFormatter.dateFormat = "MM/dd/yyyy"
         
         dateOne.text = dateFormatter.string(from: datePicker.date)
-        dateTwo.text = dateFormatter.string(from: datePicker.date)
         
         view.endEditing(true)
         
+    }
+    
+    @objc func dateChangedTwo(datePickerTwo: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        
+        
+        dateTwo.text = dateFormatter.string(from: datePickerTwo.date)
+        
+        view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
